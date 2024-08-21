2ea9685a4f5e08feca70a98a9de19a0d4ffcfd59
davide-ferrara <davide98ferrara@gmail.com>
2024-08-21 16:21:22 +0200

    first commit

--- scripts/add_new_user.sh
+++
@@ -1,7 +0,0 @@
-### POST REQUEST TO ADD NEW USER 
-
-curl -v --header "Content-Type: application/json" \
-  --request POST \
-  --data-binary @user.json \
-  http://localhost:8080/api/v1/user/register
-
